68463f6858a3599a0c8680e90b0e399c1df1fa87
maksimtarunin <tarmaksi@yandex.ru>
2022-05-22 16:49:13 +0300

    Задание по React

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const user = props.sourceId;
+    const [mess, setMess] = useState(-1);
+    const callback = (messaNow: number) => {
+        setMess(messaNow);
+    };
+    useEffect(() => {
+        subscribe(user, callback);
+        setMess(-1);
+        return () => unsubscribe(user, callback);
+    }, [user]);
+
+    return (
+        <div>
+            {user}: {mess}
+        </div>
+    );
 }
